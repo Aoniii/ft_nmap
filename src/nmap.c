277d7e08bd42edbb9000679330adc098bd62b078
Aoniii <aoniii.contact@gmail.com>
2026-06-27 22:59:42 +0200

    feat: scan configuration message

--- src/nmap.c
+++ src/nmap.c
@@ -1,7 +1,10 @@
 #include "ft_nmap.h"
+#include <stdio.h>
 
 int nmap(t_raw_data *raw, char **args) {
     t_config    cfg;
+    //t_target    *target;
+    //char        buff[INET_ADDRSTRLEN];
 
     if (build_config(raw, &cfg) == -1)
         return (-1);
@@ -11,13 +14,16 @@ int nmap(t_raw_data *raw, char **args) {
         return (-1);
     }
 
-    t_target    *node = cfg.targets;
-    char        buf[INET_ADDRSTRLEN];
-    while (node) {
-        inet_ntop(AF_INET, &node->ip, buf, sizeof(buf));
-        printf("name: %s, ip: %s\n", node->name, buf);
-        node = node->next;
-    }
+    printf("Scan Configurations\n");
+    printf("Target Ip-Address: \n");
+    printf("No of Ports to scan: %d", cfg.nb_ports);
+    printf("Scans to be performed: \n");
+    printf("No of threads: %d\n", cfg.speedup);
+    printf("Scanning...\n");
+    //while (target) {
+        //inet_ntop(AF_INET, &target->ip, buff, sizeof(buff));
+        //target = target->next;
+    //}
 
     free_target(&cfg);
     return (0);
